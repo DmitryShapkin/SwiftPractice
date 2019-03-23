7d560769f289abf43e0119b44e4c56cd2da92ec5
Dmitry Shapkin <shapkindev@gmail.com>
2019-03-23 18:18:35 +0300

    Added init commit for UICollectionView

---
+++ UICollectionView-Ray-Wenderlich-Tutorial/UICollectionView-Ray-Wenderlich-Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionView-Ray-Wenderlich-Tutorial
+//
+//  Created by Dmitry Shapkin on 23/03/2019.
+//  Copyright © 2019 Dmitry Shapkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
